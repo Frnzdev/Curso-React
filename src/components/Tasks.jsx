0eab2158dd15f8cae22d6cfc931cdfb8761385a0
Frnzdev <felipeacfornazeiro@gmail.com>
2025-03-21 20:17:05 -0300

    commit inicial

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,41 @@
+import { ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import Button from "./Button";
+
+
+function Tasks({ tasks, OnTaskClick, OnDeleteTaskClick }) {
+    const navigate = useNavigate();
+
+    function OnSeeDetailsClick(task) {
+        const query = new URLSearchParams();
+        query.set('title', task.title);
+        query.set('description', task.description)
+        navigate(`/task?${query.toString()}`)
+    }
+
+    return (
+        <div>
+            <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">{tasks.map((task) => (
+                <li key={task.id} className="flex gap-2">
+                    <button
+                        onClick={() => OnTaskClick(task.id)}
+                        className={`bg-slate-400 w-full text-white text-left p-2 rounded-md ${task.isCompleted && 'line-through'}`}
+                    >
+                        {task.title}
+
+                    </button>
+                    <Button onClick={() => OnSeeDetailsClick(task)}>
+                        <ChevronRightIcon />
+                    </Button>
+                    <Button onClick={() => OnDeleteTaskClick(task.id)}>
+                        <TrashIcon />
+                    </Button>
+                </li>
+            ))}
+            </ul>
+
+        </div >
+    )
+}
+
+export default Tasks;
